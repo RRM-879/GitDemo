3d4a936b222bb880188f10cff5053d094a0e2658
Ryan Mann <ryan.randall.mann@gmail.com>
2026-07-24 17:40:38 -0600

    Added a main to this branch of the project for the purposes of testing some git commands.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+void main() {
+    IO.println(String.format("Hello, this is my reintroduction to Git, Java and IntelliJ after a long absence."));
+
+    // The purpose of this test program is to re-learn what I've forgotten about version control, git and managing projects.
+
+    // For this, I wish to count to 10 and display the incremental counter 'i's' value
+    for (int i = 1; i <= 10; i++) {
+
+        IO.println("i = " + i);
+    }
+
+    IO.println(String.format("This has been a test of IntelliJ and Git to get it to work together."));
+}
