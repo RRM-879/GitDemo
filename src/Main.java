73bd09c469ade7c81f2c073fd701c82b89e8d26a
Ryan Mann <ryan.randall.mann@gmail.com>
2026-07-24 17:58:17 -0600

    I've now tested and added a different string formatting bit to test how I can resolve the outdated warnings.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 void main() {
-    IO.println(String.format("Hello, this is my reintroduction to Git, Java and IntelliJ after a long absence."));
+
+    // The purpose of this is to try and tackle the new format warnings for Strings and to learn something new.
+    String intro = "Hello, this is my reintroduction to Git, Java and IntelliJ after a long absence.";
+    String outro = "This has been a test of IntelliJ and Git to get it to work together";
+
+    IO.println("%s".formatted(intro));
 
     // The purpose of this test program is to re-learn what I've forgotten about version control, git and managing projects.
 
@@ -9,5 +14,6 @@ void main() {
         IO.println("i = " + (i + i));
     }
 
-    IO.println(String.format("This has been a test of IntelliJ and Git to get it to work together."));
+    // Same as the intro string but to learn if I could just initialize it and make it work in the println
+    IO.println("%s".formatted(outro));
 }
